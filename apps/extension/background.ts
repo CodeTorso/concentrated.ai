b140070b2baec36a4166f8b479fafc7ffcbd0399
codetorso <pruthvirajthinks@gmail.com>
2024-06-26 01:41:23 +0530

    Deleting tabs work

--- apps/extension/background.ts
+++ apps/extension/background.ts
@@ -1,1 +1,23 @@
 console.log('Hello from the background script!')
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension installed or updated.");
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "getTabs") {
+    console.log("get Tabs!");
+    chrome.tabs.query({}, (tabs) => {
+      sendResponse(tabs);
+    });
+    // Required to indicate you will send a response asynchronously
+    return true;
+  } else if (message.action === "removeTab" && message.tabId) {
+    console.log("Delete a Tab!");
+    chrome.tabs.remove(message.tabId, () => {
+      sendResponse({ status: "success" });
+    });
+    // chrome.tabs.
+    return true;
+  }
+});
