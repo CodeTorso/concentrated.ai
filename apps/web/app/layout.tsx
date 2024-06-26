e787acd73108a8f9f5af24c9ff9cdc1155b02d6a
codetorso <pruthvirajthinks@gmail.com>
2024-06-26 07:03:25 +0530

    better metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,8 +5,51 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Concentrated.pro",
-  description: "The problem is not you, the problem is what surrounds you",
+  title: "Concentrated.pro - get shit done.",
+  description:
+    "The problem is not you, the problem is what surrounds you",
+  openGraph: {
+    images: [
+      {
+        url: "https://concentrated.pro/og-image.png",
+        width: 2185,
+        height: 987,
+        alt: "Concentrated.pro - get shit done.",
+      },
+    ],
+  },
+  metadataBase: {
+    host: "https://concentrated.pro",
+    href: "/",
+    origin: "https://concentrated.pro",
+    password: "concentrated",
+    hash: "concentrated",
+    pathname: "/",
+    search: "",
+    username: "concentrated",
+    hostname: "concentrated.pro",
+    port: "",
+    protocol: "https:",
+    searchParams: new URLSearchParams(""),
+    toString: () => "https://concentrated.pro/",
+    toJSON: () => "https://concentrated.pro/",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "https://concentrated.pro",
+    creator: "https://concentrated.pro",
+    title: "Concentrated.pro - get shit done.",
+    description:
+      "The problem is not you, the problem is what surrounds you.",
+    images: [
+      {
+        url: "https://concentrated.pro/og-image.png",
+        width: 2185,
+        height: 987,
+        alt: "Concentrated.pro - get shit done.",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
